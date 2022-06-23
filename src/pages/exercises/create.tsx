81820839a89047eaa465913eb1c67562c1d5bc31
Jake <jake.ord345@gmail.com>
2022-06-23 14:22:12 +0200

    setup backend for exercise creation

--- src/pages/exercises/create.tsx
+++ src/pages/exercises/create.tsx
@@ -1,5 +1,22 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/router";
+import {
+  CreateExerciseType,
+  createExerciseValidator
+} from "@/shared/create-exercise-validator";
+
 const CreateExercisePage = () => {
-  return <main>CreateExercisePage</main>;
+  const { push } = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<CreateExerciseType>({
+    resolver: zodResolver(createExerciseValidator)
+  });
+
+  return <form onSubmit={handleSubmit(data => console.log(data))}></form>;
 };
 
 export default CreateExercisePage;
