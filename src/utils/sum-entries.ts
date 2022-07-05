9725eed46635bf04229e8ccca0d08a876208e320
Jake <jake.ord345@gmail.com>
2022-07-05 17:37:19 +0100

    Added recursion summary

--- src/utils/sum-entries.ts
+++ src/utils/sum-entries.ts
@@ -1,1 +1,14 @@
-export const sumWeights = (entries: Entry)
+import { EntryType } from "@/hooks/queries/validators";
+
+export const sumEntries = (entries: EntryType[]) => {
+  let sum = 0;
+
+  entries.forEach(entry => {
+    entry.sets.forEach(set => {
+      if (set.weight) sum += set.weight;
+      else if (set.distance) sum += set.distance;
+    });
+  });
+
+  return sum;
+};
