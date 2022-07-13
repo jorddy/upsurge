a5c7cf4211a0a60601bef5cecc0c216b12beb23b
Jake <jake.ord345@gmail.com>
2022-07-13 22:55:18 +0100

    finished hooks

--- src/hooks/use-search.ts
+++ src/hooks/use-search.ts
@@ -1,5 +1,5 @@
-import { InferQueryOutput } from "@/utils/trpc";
 import { useEffect, useState } from "react";
+import { InferQueryOutput } from "@/utils/trpc";
 
 export const useSearch = (
   query: string,
