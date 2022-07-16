62c64d95d21875a3659db74839a35b200fcad59a
Jake <jake.ord345@gmail.com>
2022-07-16 16:02:09 +0100

    working example to update global state

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,16 +1,25 @@
 import Header from "@/components/header";
 import { authorize } from "@/utils/authorize";
 import { useProfileStore } from "@/utils/profile-store";
+import { Switch } from "@headlessui/react";
 
 export { authorize as getServerSideProps };
 
 export default function ProfilePage() {
-  const convertWeightToKilos = useProfileStore(
-    state => state.convertWeightToKilos
-  );
-  const convertWeightToPounds = useProfileStore(
-    state => state.convertWeightToPounds
-  );
+  const { weightUnit, convertWeightToKilos, convertWeightToPounds } =
+    useProfileStore();
+
+  console.log(weightUnit);
+
+  const handleWeightChange = () => {
+    if (weightUnit === "kg") {
+      convertWeightToPounds();
+    }
+
+    if (weightUnit === "lbs") {
+      convertWeightToKilos();
+    }
+  };
 
   return (
     <>
@@ -18,7 +27,28 @@ export default function ProfilePage() {
 
       <main className='container mx-auto p-4 space-y-4'>
         <h1 className='text-2xl font-bold'>Your profile</h1>
-        <section className='p-4 bg-zinc-900 border border-zinc-500'></section>
+        <section className='p-4 bg-zinc-900 border border-zinc-500'>
+          <Switch.Group>
+            <div className='flex items-center'>
+              <Switch.Label className='pr-4'>
+                Switch weight unit to pounds (lbs)
+              </Switch.Label>
+              <Switch
+                checked={weightUnit === "lbs"}
+                onChange={handleWeightChange}
+                className={`${
+                  weightUnit === "lbs" ? "bg-blue-600" : "bg-gray-200"
+                } relative inline-flex h-6 w-11 items-center rounded-full transition-colors focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
+              >
+                <span
+                  className={`${
+                    weightUnit === "lbs" ? "translate-x-6" : "translate-x-1"
+                  } inline-block h-4 w-4 transform rounded-full bg-white transition-transform`}
+                />
+              </Switch>
+            </div>
+          </Switch.Group>
+        </section>
       </main>
     </>
   );
