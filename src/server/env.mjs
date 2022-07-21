53bd40c61c22e12a33b0fb23e0250486b579ef88
Jake <jake.ord345@gmail.com>
2022-07-21 23:02:15 +0100

    made env vars beter

--- src/server/env.mjs
+++ src/server/env.mjs
@@ -4,14 +4,16 @@ const envValidator = z.object({
   DATABASE_URL: z.string(),
   GOOGLE_ID: z.string(),
   GOOGLE_SECRET: z.string(),
-  NEXTAUTH_URL: z.string(),
+  NEXTAUTH_URL: z.string().optional(),
   NEXTAUTH_SECRET: z.string()
 });
 
 const validateEnv = envValidator.safeParse(process.env);
 
 if (!validateEnv.success) {
-  throw new Error("Invalid env variables");
+  throw new Error(
+    Object.entries(validateEnv.error.flatten().fieldErrors).join(", ")
+  );
 }
 
 export const env = validateEnv.data;
